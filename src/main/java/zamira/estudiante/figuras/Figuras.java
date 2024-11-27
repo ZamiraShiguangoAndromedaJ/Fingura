5e68d40d2ff59f9f7a266ba5468960b1b557f1ce
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-11-27 15:13:03 -0500

    Creación del proyecto

---
+++ src/main/java/zamira/estudiante/figuras/Figuras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package zamira.estudiante.figuras;
+
+/**
+ *
+ * @author user
+ */
+public class Figuras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
